3b179e64c480088dd75a888b460f7c926f9e609f
Elite <kimo123kimo321@gmail.com>
2026-01-19 20:49:22 +0200

    addRandomWord

--- project-00022/src/App.jsx
+++ project-00022/src/App.jsx
@@ -1,9 +1,9 @@
 import {useState} from "react";
 import { languages } from "./languages"
-
+import {getFarewellText , getRandomWord} from "./utils.js";
 
 export default function App() {
-  const [currentWord, setCurrentWord] = useState("react")
+  const [currentWord, setCurrentWord] = useState(getRandomWord())
     const [guessedLetters, setGuessedLetters] = useState([])
     console.log(guessedLetters)
 
@@ -14,14 +14,15 @@ const wrongGuessCount = guessedLetters.filter(letter => !currentWord.includes(le
     const gameWon = currentWord.split("").every(letter => guessedLetters.includes((letter)))
     const gameOver = gameLost || gameWon
 
+    const lastGuessedLetter = guessedLetters[guessedLetters.length - 1]
+    const isLastGuessIncorrect = lastGuessedLetter && !currentWord.includes(lastGuessedLetter)
+    console.log(isLastGuessIncorrect)
+
     function handleGuessedLetters(letter) {
         setGuessedLetters(prevLetters =>
             prevLetters.includes(letter) ? [...prevLetters]
                 : [...prevLetters, letter])
     }
-
-
-
 
 
 const languagesEle = languages.map((lang ,i) => {
@@ -32,7 +33,9 @@ const languagesEle = languages.map((lang ,i) => {
 
     const langIsLost = i < wrongGuessCount;
 
-  return (
+
+
+    return (
     <span
 
       key={lang.name}
@@ -75,7 +78,9 @@ const languagesEle = languages.map((lang ,i) => {
                       ? "bg-green-500"
                       : "bg-red-500"
                   : "bg-[#FCBA29] hover:bg-yellow-400"
+              
           }
+          ${gameOver ? "opacity-60 cursor-not-allowed pointer-events-none" : ""}
 `}
 
     onClick={() => handleGuessedLetters(letter)}
@@ -91,6 +96,19 @@ const languagesEle = languages.map((lang ,i) => {
     </header>
       {gameWon ? <section className="text-center border-s-[#10A95B] bg-[#10A95B] rounded-sm text-xl  text-[#F9F4DA] mt-5 w-88 h-14 ">you win! <br /> well done🎉</section> :null}
       {gameLost ? <section className="text-center border-s-[#BA2A2A] bg-[#BA2A2A] rounded-sm text-xl  text-[#F9F4DA] mt-5 w-full ">Game over! <br /> You lose! Better start learning Assembly 😭</section> :null}
+      {isLastGuessIncorrect ? <section className="
+       mt-5 w-full
+      bg-[#7A5EA7]
+      text-[#F9F4DA]
+      text-xl
+      text-center
+      rounded-md
+      border
+      border-dashed
+    border-[#323232]
+      px-6 py-4
+      shadow-md
+      italic ">{getFarewellText(languages[wrongGuessCount - 1].name)}</section> : null}
       <section className="flex flex-wrap justify-center gap-2 pt-8 mt-6 max-w-md mx-auto">
   {languagesEle}
 </section>
@@ -99,7 +117,7 @@ const languagesEle = languages.map((lang ,i) => {
   {letterElements}
 </section>
 
-    <section className="flex flex-wrap justify-center gap-2 max-w-[450px] pt-12
+    <section className="flex flex-wrap justify-center gap-2 max-w-[450px] pt-12 cursor-pointer
  ">{lettersMapping}</section>
 
     <section className="flex justify-center pt-4 mt-6 max-w-md mx-auto" >
